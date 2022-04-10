a368631d26f8bcab62e5b12b966eac8f6c485c5c
nwagu chima <chima.nwagu@decagon.dev>
2022-04-10 23:53:44 +0100

    completed the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
 import styles from "./App.module.scss";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className={styles.App}>Hello World</div>;
+  return (
+    <div className={styles.App}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
