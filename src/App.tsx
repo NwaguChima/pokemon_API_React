a36297f7ecf7c767f8e607d678a88520abfa51d4
nwagu chima <chima.nwagu@decagon.dev>
2022-04-11 02:17:08 +0100

    updated modal styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { useRef } from "react";
 import styles from "./App.module.scss";
 import Container from "./components/Container/Container";
 import Filter from "./components/Filter/Filter";
 import Header from "./components/Header/Header";
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const modalRef = useRef();
   return (
     <div className={styles.App}>
       <Header />
       <Filter />
+      <Modal ref={modalRef} />
       <Container />
     </div>
   );
